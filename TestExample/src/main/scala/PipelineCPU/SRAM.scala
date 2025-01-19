760c2b326923443197d2fa0b45e5c3766cc5d45c
Bobby <bobby@Ubuntu.myguest.virtualbox.org>
2025-01-19 11:10:36 +0800

    All_CPU_submodule_is_done_!!!

--- TestExample/src/main/scala/PipelineCPU/SRAM.scala
+++ TestExample/src/main/scala/PipelineCPU/SRAM.scala
@@ -11,7 +11,7 @@ class SRAM extends Module {
 
   val mem = Mem(65536, UInt(8.W))
 
-  // 寫入邏輯
+  // write
   when(io.w_en =/= 0.U) {
     when(io.w_en(0)) { mem.write(io.address, io.write_data(7, 0)) }
     when(io.w_en(1)) { mem.write(io.address + 1.U, io.write_data(15, 8)) }
@@ -19,14 +19,14 @@ class SRAM extends Module {
     when(io.w_en(3)) { mem.write(io.address + 3.U, io.write_data(31, 24)) }
   }
 
-  // 讀取邏輯
+  // read logic
   val read_data_bytes = VecInit(Seq.fill(4)(0.U(8.W)))
   read_data_bytes(0) := mem.read(io.address)
   read_data_bytes(1) := mem.read(io.address + 1.U)
   read_data_bytes(2) := mem.read(io.address + 2.U)
   read_data_bytes(3) := mem.read(io.address + 3.U)
 
-  io.read_data := Cat(read_data_bytes.reverse) // 將讀取的位元組組合成 32 位元輸出
+  io.read_data := Cat(read_data_bytes.reverse) 
 }
 
 
